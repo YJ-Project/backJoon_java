8b0183857ca71d4c45f2e45f9fdcf83b7335a22e
yujin <leeyujin95y@naver.com>
2020-05-05 12:27:54 +0900

    2742번 기찍 N

---
+++ backJoon_java/src/code/backJoon_2742.java
@@ -0,0 +1,15 @@
+package code;
+
+import java.util.Scanner;
+
+public class backJoon_2742 {
+
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int N = Integer.parseInt(scan.nextLine());
+		scan.close();
+		for(int i=N ; i > 0 ; i--){
+			System.out.println(i);
+		}
+	}
+}
