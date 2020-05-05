1ca108eb3c74891bb307501cb2289da18fb24cbd
yujin <leeyujin95y@naver.com>
2020-05-05 12:33:44 +0900

    11021번 A+B -7

---
+++ backJoon_java/src/code/backJoon_11021.java
@@ -0,0 +1,17 @@
+package code;
+
+import java.util.Scanner;
+
+public class backJoon_11021 {
+
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int T = Integer.parseInt(scan.nextLine());
+		for(int i=0 ; i < T ; i++){
+			int a = Integer.parseInt(scan.next());
+			int b = Integer.parseInt(scan.next());
+			System.out.println("Case #"+(i+1)+": "+(a+b));
+		}
+		scan.close();
+	}
+}
