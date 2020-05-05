f7140114d9ec3555b8781ffa5edf57889b8f0388
yujin <leeyujin95y@naver.com>
2020-05-05 13:16:06 +0900

    2439번 별찍기 -2

---
+++ backJoon_java/src/code/backJoon_2439.java
@@ -0,0 +1,21 @@
+package code;
+
+import java.util.Scanner;
+
+public class backJoon_2439 {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int T = Integer.parseInt(scan.nextLine());
+		for(int i=1 ; i < T+1 ; i++){
+			for(int k=i; k<T ; k++){
+				System.out.print(" ");
+			}
+			
+			for(int j=0; j<i ; j++){
+				System.out.print("*");	
+			}
+			System.out.println();
+		}
+		scan.close();
+	}
+}
