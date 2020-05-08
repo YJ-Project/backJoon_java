163961887bce4afc17d11f38687cb7e4f5e0a3f2
yujin <leeyujin95y@naver.com>
2020-05-08 10:35:42 +0900

    10951번 A+B - 4

---
+++ backJoon_java/src/code/backJoon_10951.java
@@ -0,0 +1,17 @@
+package code;
+
+import java.util.Scanner;
+
+public class backJoon_10951 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		while(scan.hasNextInt()){
+			int a = scan.nextInt();
+			int b = scan.nextInt();
+			System.out.println(a+b);
+		}
+		scan.close();
+	}
+}
