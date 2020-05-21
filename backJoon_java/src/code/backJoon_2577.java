ec184629474814937625b8cb080170345dee8c7a
yujin <leeyujin95y@naver.com>
2020-05-21 11:08:26 +0900

    2577번 숫자의 개수

---
+++ backJoon_java/src/code/backJoon_2577.java
@@ -0,0 +1,25 @@
+package code;
+
+import java.util.Scanner;
+
+public class backJoon_2577 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int c = scan.nextInt();
+		int x = a*b*c;
+		int[] result = new int[10];
+		scan.close();
+		
+		while(x > 0){
+			result[x%10]++;
+			x = x/10;
+		}
+		
+		for(int i=0; i<result.length;i++){
+			System.out.println(result[i]);
+		}
+	}
+}
