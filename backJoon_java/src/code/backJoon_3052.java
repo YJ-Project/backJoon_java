8b5e0b5947cad773cdf5e5270c6178e014b91e25
yujin <leeyujin95y@naver.com>
2020-05-22 13:14:39 +0900

    3052번 나머지

---
+++ backJoon_java/src/code/backJoon_3052.java
@@ -0,0 +1,18 @@
+package code;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class backJoon_3052 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		HashSet<Integer> set = new HashSet<>(); 
+		for(int i=0; i<10;i++){
+			set.add(scan.nextInt()%42);
+		}
+		System.out.println(set.size());
+		scan.close();
+
+	}
+}
