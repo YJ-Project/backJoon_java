dcffc16326d9b79f244cc6951064fe0e062d3087
yujin <leeyujin95y@naver.com>
2020-06-01 21:08:14 +0900

    1546번 평균 int -> double로 변경

--- backJoon_java/src/code/backJoon_1546.java
+++ backJoon_java/src/code/backJoon_1546.java
@@ -6,9 +6,9 @@ public class backJoon_1546 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		int n = scan.nextInt();
-		int score = 0;
-		int max = 0;
-		int ave = 0;
+		double score = 0;
+		double max = 0;
+		double ave = 0;
 		for(int i=0; i<n; i++){
 			score=scan.nextInt();
 			ave += score; 
@@ -16,7 +16,7 @@ public class backJoon_1546 {
 				max = score;
 			}
 		}
-		System.out.println((double)ave/(double)max*100/n);
+		System.out.println(ave/max*100/n);
 		scan.close();
 	}
 }
