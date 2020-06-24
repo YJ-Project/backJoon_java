12ddf5fe9133b3e690816db315299a1c1ccddf6a
yujin <leeyujin95y@naver.com>
2020-06-24 12:13:36 +0900

    1065번 한수

---
+++ backJoon_java/src/code/backJoon_1065.java
@@ -0,0 +1,25 @@
+package code;
+
+import java.util.Scanner;
+
+public class backJoon_1065 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		scan.close();
+		int result = 0;
+		
+		if(n<100){
+			result=n;
+		}else{
+			result = 99;
+			
+			for(int i=100; i<=n;i++){
+				if((i/10%10)*2 == i/100+i%10){
+					result++;
+				}
+			}
+		}
+		System.out.println(result);
+	}
+}
