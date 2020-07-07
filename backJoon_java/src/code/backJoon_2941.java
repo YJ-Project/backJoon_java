7a0387de77fc979af793a29c1722fe3496eaabd3
yujin <leeyujin95y@naver.com>
2020-07-07 20:49:46 +0900

    2941번 크로아티아 알파벳

---
+++ backJoon_java/src/code/backJoon_2941.java
@@ -0,0 +1,22 @@
+package code;
+
+import java.util.Scanner;
+
+public class backJoon_2941 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String alphabat = scan.nextLine();
+		int len = 0;
+		scan.close();
+		String[] c = {"c=","c-","dz=","d-","lj","nj","s=","z="};
+		for(int i=0; i<c.length;i++){
+			while(alphabat.contains(c[i])){
+				len++;
+				alphabat = alphabat.replaceFirst(c[i], " ");
+			}
+		}
+		System.out.println(alphabat.length());
+	}
+
+}
