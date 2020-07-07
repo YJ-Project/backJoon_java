bd03d224622c351837eec31bcb590def3ddeaefb
yujin <leeyujin95y@naver.com>
2020-07-07 20:50:56 +0900

    2941번 크로아티아 알파벳(사용하지 않는 변수 제거)

--- backJoon_java/src/code/backJoon_2941.java
+++ backJoon_java/src/code/backJoon_2941.java
@@ -7,12 +7,10 @@ public class backJoon_2941 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String alphabat = scan.nextLine();
-		int len = 0;
 		scan.close();
 		String[] c = {"c=","c-","dz=","d-","lj","nj","s=","z="};
 		for(int i=0; i<c.length;i++){
 			while(alphabat.contains(c[i])){
-				len++;
 				alphabat = alphabat.replaceFirst(c[i], " ");
 			}
 		}
