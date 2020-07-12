6134dbeaee08ad683e4afd099af2adbe417b9216
yujin <leeyujin95y@naver.com>
2020-07-12 19:48:25 +0900

    2869번 달팽이는 올라가고 싶다

---
+++ backJoon_java/src/code/backJoon_2869.java
@@ -0,0 +1,16 @@
+package code;
+
+import java.util.Scanner;
+
+public class backJoon_2869 {
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int a = scan.nextInt();
+		int b = scan.nextInt();
+		int v = scan.nextInt();
+		int result = (v-a)/(a-b)+1;
+		int today = (v-a)%(a-b) ==0?result:result+1;
+		System.out.println(today);
+		scan.close();
+	}
+}
