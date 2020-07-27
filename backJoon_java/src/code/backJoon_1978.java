762c0cc650cff404319ac01ba190dd92b8e8127e
lyj <leeyujin95y@naver.com>
2020-07-27 19:58:23 +0900

    1978번 소수 찾기

---
+++ backJoon_java/src/code/backJoon_1978.java
@@ -0,0 +1,25 @@
+package code;
+
+import java.util.Scanner;
+
+public class backJoon_1978 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = Integer.parseInt(scan.nextLine());
+		int result = n;
+		for(int i=0; i<n; i++){
+			int temp = scan.nextInt();
+				if(temp==1) result--;
+				for(int j=2;j<temp;j++){
+					if(temp%j==0){
+						result--;
+						break;
+					}
+				}
+		}
+		System.out.println(result);
+		scan.close();
+	}
+
+}
